adc522f3fedd2e9a3956ff2d4137cc1ddf7c334a
Paulo-Gouvea <paulogouvea1994@gmail.com>
2022-02-19 18:42:20 -0300

    start: start of the app

---
+++ App.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { StatusBar } from 'react-native';
+
+import { useFonts, Lato_400Regular, Lato_700Bold } from '@expo-google-fonts/lato';
+import { SourceSansPro_400Regular } from '@expo-google-fonts/source-sans-pro';
+import AppLoading from 'expo-app-loading';
+
+import { ThemeProvider } from 'styled-components/native';
+import theme from './src/theme';
+
+import { Home } from './src/screens/Home';
+
+export default function App() {
+  const [fontsLoaded] = useFonts({
+    Lato_400Regular,
+    Lato_700Bold,
+    SourceSansPro_400Regular
+  });
+
+  if(!fontsLoaded){
+    return <AppLoading />
+  }
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Home />
+    </ThemeProvider>
+  );
+}
+
---
+++ src/theme/index.ts
@@ -0,0 +1,15 @@
+export default {
+    colors: {
+        white: '#FFFFFF',
+        black: '#25282B',
+
+        blue: '#4485FD',
+        green: '#00CC6A',
+    },
+
+    fonts: {
+        lato_regular: 'Lato_400Regular',
+        lato_bold: 'Lato_700Bold',
+        sourcesanspro_regular: 'SourceSansPro_400Regular'
+    }
+}
